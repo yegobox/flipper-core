624431906354b04b96d79a1272d54a522990aa82
Richard Muragijimana <beastar457@gmail.com>
2021-03-07 11:25:50 +0200

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        //
+        //TODO: make new design.
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
