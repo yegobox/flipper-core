26d47aef919feea36ae80f8daa7982425e307642
Richard Muragijimana <beastar457@gmail.com>
2021-03-09 14:11:56 +0200

    enable

--- packages/flipper_login/lib/flipper_login.dart
+++ packages/flipper_login/lib/flipper_login.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Text('Welcome to desktop'));
