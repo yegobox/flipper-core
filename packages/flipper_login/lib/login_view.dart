279ef1b6cd97efa1898359b0bbc4019978c6215b
Yvan Valens <nsengvalens4@gmail.com>
2021-03-11 17:08:18 +0200

    feat(new design): Landing page design has to be changing according to the selected platform

--- packages/flipper_login/lib/login_view.dart
+++ packages/flipper_login/lib/login_view.dart
@@ -14,16 +14,31 @@ class LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     if (UniversalPlatform.isWeb) {
-      return Scaffold(body: Text('Web'));
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('AppBar demo'),
+          ),
+          body: Text('web'));
     } else if (UniversalPlatform.isWindows ||
         UniversalPlatform.isLinux ||
         UniversalPlatform.isMacOS) {
-      return Scaffold(body: Text('Desktop'));
-    } else if (UniversalPlatform.isAndroid) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('AppBar demo'),
+          ),
+          body: Text('Desktop'));
     } else if (UniversalPlatform.isIOS) {
-      return Scaffold(body: Text('ios'));
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('AppBar demo'),
+          ),
+          body: Text('Ios'));
     } else if (UniversalPlatform.isAndroid) {
-      return Scaffold(body: Text('android'));
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('AppBar demo'),
+          ),
+          body: Text('android'));
     }
     return Scaffold(body: SizedBox.shrink()); //nothing to be returned.
   }
