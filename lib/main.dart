49f2e42a0a58ac9508069ce3ddf4fd7489419026
Yvan Valens <nsengvalens4@gmail.com>
2021-03-11 17:08:18 +0200

    feat(new design): Landing page design has to be changing according to the selected platform

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:flipper_login/login_view.dart';
 
 void main() {
-  runApp(FlipperApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: SafeArea(
+          top: false,
+          child: LoginView(),
+        ),
+      )
+    );
+  }
 }
--- packages/flipper_login/lib/login_view.dart
+++ packages/flipper_login/lib/login_view.dart
@@ -1,5 +1,7 @@
 library flipper_login;
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:universal_platform/universal_platform.dart';
 
@@ -13,18 +15,68 @@ class LoginView extends StatefulWidget {
 class LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    if (UniversalPlatform.isWeb) {
-      return Scaffold(body: Text('Web'));
-    } else if (UniversalPlatform.isWindows ||
-        UniversalPlatform.isLinux ||
-        UniversalPlatform.isMacOS) {
-      return Scaffold(body: Text('Desktop'));
-    } else if (UniversalPlatform.isAndroid) {
-    } else if (UniversalPlatform.isIOS) {
-      return Scaffold(body: Text('ios'));
-    } else if (UniversalPlatform.isAndroid) {
-      return Scaffold(body: Text('android'));
-    }
-    return Scaffold(body: SizedBox.shrink()); //nothing to be returned.
+
+      return Column(
+        children: <Widget>[
+          Container(
+              color: Colors.blue[100],
+              width: double.infinity,
+              height: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset('assets/fliper-logo.png', width: 80, height: 80,),
+
+                  SizedBox(height: 3,),
+
+                  Text('Flipper', textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 26, color: Colors.blue[900]),),
+
+                  SizedBox(height: 10,),
+
+                  Text('Interact and grow your bussiness', textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 18, color: Colors.grey[700]),),
+                ],
+              ),
+            ),
+
+          SizedBox(height: 10,),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    width: double.infinity,
+                    child: RaisedButton(
+                      padding: EdgeInsets.all(15),
+                      color: Colors.blue[900],
+                      child: Text("Create Account",style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18
+                      ),),
+                      onPressed: (){},
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  SizedBox(
+                    width: double.infinity,
+                    child: RaisedButton(
+                      padding: EdgeInsets.all(15),
+                      child: Text("Sign In",style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18
+                      ),),
+                      onPressed: (){},
+                    ),
+                  )
+                ]
+            ),
+          ),
+
+        ],
+      );
   }
 }
