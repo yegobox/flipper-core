00e219573b107c6703d03771bb3edb05a6673bdc
Richard Muragijimana <beastar457@gmail.com>
2021-03-13 12:28:05 +0200

    API proxy example

---
+++ packages/flipper_services/lib/abstractions/api.dart
@@ -0,0 +1,6 @@
+abstract class Api {
+  // Home screen futures api calls
+
+  // ignore: always_specify_types
+  Future? payroll();
+}
---
+++ packages/flipper_services/lib/third_party_services_module.dart
@@ -0,0 +1,32 @@
+import 'package:flipper_services/abstractions/api.dart';
+import 'package:flipper_services/api/fake_api.dart';
+import 'package:flipper_services/api/http_api.dart';
+import 'package:injectable/injectable.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+enum ApiProvider {
+  Fake,
+  Rest,
+}
+
+@module
+abstract class ThirdPartyServicesModule {
+  @lazySingleton
+  Api get apiService {
+    // ignore: prefer_typing_uninitialized_variables
+    var apiService;
+    const ApiProvider choosenProvider =
+        ApiProvider.Fake; // change this to change the proider
+    switch (choosenProvider) {
+      case ApiProvider.Fake:
+        apiService = FakeApi();
+        break;
+      case ApiProvider.Rest:
+        apiService = HttpApi();
+        break;
+      default:
+        apiService = HttpApi();
+    }
+    return apiService;
+  }
+}
