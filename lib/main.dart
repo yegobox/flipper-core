413176a3eecb194aba3104a0ead1b97a4aacd61b
Richard Muragijimana <beastar457@gmail.com>
2021-03-16 23:41:32 +0200

    merging

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,5 @@ import 'package:flutter/material.dart';
 
 void main() {
   setupLocator();
-<<<<<<< HEAD
   runApp(FlipperApp());
-=======
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        canvasColor: Colors.transparent,
-      ),
-      home: LoginView(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
->>>>>>> 5ad2c10139ced5296d4ce632e3c08adb016bfb36
 }
