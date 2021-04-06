a111254a6ebb46d091a3c5b20f102408054de688
dushimeemma <64155652+dushimeemma@users.noreply.github.com>
2021-04-06 18:59:27 +0200

    [WIP]#57059475 implement bottom modal with country picker (#20)
    * feat(UI): country picker popup
    
    * feat(UI): login popup view

--- packages/flipper_login/lib/login_popup_view.dart
+++ packages/flipper_login/lib/login_popup_view.dart
@@ -2,6 +2,7 @@ library flipper_login_popup;
 
 import 'package:flipper/proxy.dart';
 import 'package:flutter/material.dart';
+import 'package:country_code_picker/country_code_picker.dart';
 
 import './button_view.dart';
 
@@ -53,21 +54,36 @@ class LoginPopupView extends StatelessWidget {
             ),
             Container(
               margin: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-              child: const TextField(
-                autocorrect: true,
-                decoration: InputDecoration(
-                  hintText: '+250789078834',
-                  prefixIcon: Icon(Icons.phone),
-                  hintStyle: TextStyle(color: Colors.grey),
-                  filled: true,
-                  fillColor: Colors.white70,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
+              child: Form(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    borderSide: BorderSide(color: Colors.green, width: 2),
+                  child: Row(
+                    children: <Widget>[
+                      CountryCodePicker(
+                        onChanged: null,
+                        initialSelection: 'RW',
+                        favorite: ['+250', 'RW'],
+                        showCountryOnly: false,
+                        showOnlyCountryWhenClosed: false,
+                        alignLeft: false,
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            hintText: '789078834',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -83,8 +99,8 @@ class LoginPopupView extends StatelessWidget {
               child: ButtonView(
                 'Verify',
                 () {
-                  // Navigator.of(context).pop();
-                  ProxyService.api.webDesktopLogin(number: '78347');
+                  Navigator.of(context).pop();
+                  // ProxyService.api.webDesktopLogin(number: '78347');
                 },
                 Colors.white,
                 Colors.blue,
--- packages/flipper_login/lib/login_view.dart
+++ packages/flipper_login/lib/login_view.dart
@@ -9,12 +9,15 @@ import './login_popup_view.dart';
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
         children: <Widget>[
           Container(
             color: Colors.blue[100],
             width: double.infinity,
+            padding: EdgeInsets.all(10),
             height: 400,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -25,27 +28,21 @@ class LoginView extends StatelessWidget {
                   width: 80,
                   height: 80,
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 3,
                 ),
                 Text(
                   'Flipper',
                   textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 26,
-                      color: Colors.blue[900],
-                      decoration: TextDecoration.none),
+                  style: TextStyle(fontSize: 26, color: Colors.blue[900]),
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 10,
                 ),
                 Text(
                   'Interact and grow your bussiness',
                   textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey[700],
-                      decoration: TextDecoration.none),
+                  style: TextStyle(fontSize: 18, color: Colors.grey[700]),
                 ),
               ],
             ),
@@ -53,51 +50,50 @@ class LoginView extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      // padding: EdgeInsets.all(15),
-                      // color: Colors.blue[900],
-                      child: const Text(
-                        'Create Account',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            decoration: TextDecoration.none),
-                      ),
-                      onPressed: () {
-                        showModalBottomSheet(
-                          context: context,
-                          builder: (context) => LoginPopupView(),
-                        );
-                      },
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    // padding: EdgeInsets.all(15),
+                    // color: Colors.blue[900],
+                    child: const Text(
+                      'Create Account',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          decoration: TextDecoration.none),
                     ),
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => LoginPopupView(),
+                      );
+                    },
                   ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      // padding: EdgeInsets.all(15),
-                      child: const Text(
-                        'Sign In',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            decoration: TextDecoration.none),
-                      ),
-                      onPressed: () {
-                        showModalBottomSheet(
-                          context: context,
-                          builder: (context) => LoginPopupView(),
-                        );
-                      },
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: RaisedButton(
+                    // padding: EdgeInsets.all(15),
+                    color: Colors.white,
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                          decoration: TextDecoration.none),
                     ),
-                  )
-                ]),
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => LoginPopupView(),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
