0ed0bfeef23461333314c6031429cb917c42616d
Richard Muragijimana <beastar457@gmail.com>
2021-04-06 19:51:05 +0200

    fixing compatibility issue

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,19 +1,20 @@
 import 'package:auto_route/annotations.dart';
-import 'package:flipper_chat/screens/welcome/welcome_screen.dart';
-// import 'package:flipper_login/login_view.dart';
+// import 'package:flipper_chat/screens/welcome/welcome_screen.dart';
+import 'package:flipper_login/login_view.dart';
 import 'package:flipper_dashboard/flipper_dashboard.dart';
 
 // im
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
-    // AutoRoute(page: LoginView, initial: true),
-    AutoRoute(page: WelcomeScreen, initial: true),
+    AutoRoute(page: LoginView, initial: true),
+    // AutoRoute(page: WelcomeScreen, initial: true),
     AutoRoute(page: DashboardView),
   ],
 )
 class $AppRouter {}
 
 // ./flutterw packages pub run build_runner build --delete-conflicting-outputs
+// flutter packages pub run build_runner build --delete-conflicting-outputs
 //  firebase deploy --only hosting
 // ./flutterw build web
--- lib/routes.gr.dart
+++ lib/routes.gr.dart
@@ -5,43 +5,33 @@
 // **************************************************************************
 
 import 'package:auto_route/auto_route.dart' as _i1;
-import 'package:flipper_chat/screens/welcome/welcome_screen.dart' as _i2;
 import 'package:flipper_dashboard/flipper_dashboard.dart' as _i3;
+import 'package:flipper_login/login_view.dart' as _i2;
 
 class AppRouter extends _i1.RootStackRouter {
   AppRouter();
 
   @override
   final Map<String, _i1.PageFactory> pagesMap = {
-    WelcomeScreen.name: (entry) {
-      return _i1.MaterialPageX(
-          entry: entry,
-          child: _i2.WelcomeScreen(),
-          maintainState: true,
-          fullscreenDialog: false);
+    LoginView.name: (entry) {
+      return _i1.MaterialPageX(entry: entry, child: _i2.LoginView());
     },
     DashboardView.name: (entry) {
-      return _i1.MaterialPageX(
-          entry: entry,
-          child: _i3.DashboardView(),
-          maintainState: true,
-          fullscreenDialog: false);
+      return _i1.MaterialPageX(entry: entry, child: _i3.DashboardView());
     }
   };
 
   @override
   List<_i1.RouteConfig> get routes => [
-        _i1.RouteConfig(WelcomeScreen.name,
-            path: '/', fullMatch: false, usesTabsRouter: false),
-        _i1.RouteConfig(DashboardView.name,
-            path: '/dashboard-view', fullMatch: false, usesTabsRouter: false)
+        _i1.RouteConfig(LoginView.name, path: '/'),
+        _i1.RouteConfig(DashboardView.name, path: '/dashboard-view')
       ];
 }
 
-class WelcomeScreen extends _i1.PageRouteInfo {
-  const WelcomeScreen() : super(name, path: '/');
+class LoginView extends _i1.PageRouteInfo {
+  const LoginView() : super(name, path: '/');
 
-  static const String name = 'WelcomeScreen';
+  static const String name = 'LoginView';
 }
 
 class DashboardView extends _i1.PageRouteInfo {
