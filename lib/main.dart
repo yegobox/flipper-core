afc857ccad1fb775f32e6dc44a82ac4e10cd72cb
valens10 <38139189+valens10@users.noreply.github.com>
2021-04-09 19:09:20 +0200

    #2 Web and desktop landing page (#18)
    * feat(landing-page): web and desktop landing page
    
    * Add the footer section where the image can be added
    
    Co-authored-by: Muragijimana <beastar457@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,20 @@ void main() {
   // setupLocator();
   runApp(FlipperApp());
 }
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+            toggleableActiveColor: Colors.green
+        ),
+        home: Scaffold(
+          backgroundColor: Colors.grey[300],
+          body: LoginView(),
+        )
+    );
+  }
+}
